69561f80ac515dd78498cad3e067697d12596057
anastasia-starostina <anastasia.st.stern@gmail.com>
2023-05-27 15:55:20 +0100

    Separate GET api into a separate router in tranaction.js file

--- server/app.js
+++ server/app.js
@@ -2,82 +2,24 @@ const axios = require('axios');
 const cors = require('cors');
 const express = require('express');
 require('dotenv').config();
+const transactionsRouter = require('./routes/transactions');
+
 const app = express();
 const PORT = 3000;
 
-// Define a data structure to store the transactions
-let transactions = [];
+// Use the transactions router as middleware
+app.use('/api/transactions', transactionsRouter);
 
 // Enable CORS
 app.use(cors());
 
-// Define a route to retrieve the transactions
-app.get('/api/transactions', async (req, res) => {
-  try {
-    //Retrieve the filter parameters from the request query string
-    const { fromAddress, toAddress, aboveValue, belowValue, limit, offset } =
-      req.query;
-    // Define the Etherscan API URL and retrieve the API key from environment variables
-    const apiUrl = 'https://api.etherscan.io/api';
-    const apiKey = process.env.ETHERSCAN_API_KEY;
-
-    const response = await axios.get(apiUrl, {
-      params: {
-        module: 'account',
-        action: 'tokentx',
-        contractaddress: '0x9355372396e3F6daF13359B7b607a3374cc638e0',
-        page: 1,
-        sort: 'asc',
-        offset: 100,
-        apikey: apiKey,
-      },
-    });
-
-    // Store the transactions in memory
-    transactions = response.data.result;
-
-    // Apply filters
-    let filteredTransactions = transactions;
-    if (fromAddress) {
-      filteredTransactions = filteredTransactions.filter(
-        tx => tx.from === fromAddress,
-      );
-    }
-    if (toAddress) {
-      filteredTransactions = filteredTransactions.filter(
-        tx => tx.to === toAddress,
-      );
-    }
-    if (aboveValue) {
-      filteredTransactions = filteredTransactions.filter(
-        tx => +tx.value > +aboveValue,
-      );
-    }
-    if (belowValue) {
-      filteredTransactions = filteredTransactions.filter(
-        tx => +tx.value < +belowValue,
-      );
-    }
-    if (limit) {
-      filteredTransactions = filteredTransactions.slice(0, +limit);
-    }
-    if (offset) {
-      filteredTransactions = filteredTransactions.slice(+offset);
-    }
-    res.json(filteredTransactions);
-
-    //Handle errors
-    if (response.data.status === '0') {
-      console.log(response.data.status);
-      return { status: 404, message: response.data.result };
-    } else {
-      return { status: 200, message: response.data };
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'An error occurred' });
-  }
+app.get('/', function (req, res) {
+  res.json({
+    success: true,
+    message: 'Test route up and running!',
+  });
 });
+
 
 app.listen(PORT, error => {
   if (!error)
---
+++ server/routes/transactions.js
@@ -0,0 +1,77 @@
+const axios = require('axios');
+const express = require('express');
+const router = express.Router();
+
+// Define a route to retrieve the transactions
+router.get('/', async (req, res) => {
+  // Define a data structure to store the transactions
+  let transactions = [];
+
+  // Handle the GET request for transactions
+  try {
+    //Retrieve the filter parameters from the request query string
+    const { fromAddress, toAddress, aboveValue, belowValue, limit, offset } =
+      req.query;
+    // Define the Etherscan API URL and retrieve the API key from environment variables
+    const apiUrl = 'https://api.etherscan.io/api';
+    const apiKey = process.env.ETHERSCAN_API_KEY;
+
+    const response = await axios.get(apiUrl, {
+      params: {
+        module: 'account',
+        action: 'tokentx',
+        contractaddress: '0x9355372396e3F6daF13359B7b607a3374cc638e0',
+        page: 1,
+        sort: 'asc',
+        offset: 100,
+        apikey: apiKey,
+      },
+    });
+
+    // Store the transactions in memory
+    transactions = response.data.result;
+
+    // Apply filters
+    let filteredTransactions = transactions;
+    if (fromAddress) {
+      filteredTransactions = filteredTransactions.filter(
+        tx => tx.from === fromAddress,
+      );
+    }
+    if (toAddress) {
+      filteredTransactions = filteredTransactions.filter(
+        tx => tx.to === toAddress,
+      );
+    }
+    if (aboveValue) {
+      filteredTransactions = filteredTransactions.filter(
+        tx => +tx.value > +aboveValue,
+      );
+    }
+    if (belowValue) {
+      filteredTransactions = filteredTransactions.filter(
+        tx => +tx.value < +belowValue,
+      );
+    }
+    if (limit) {
+      filteredTransactions = filteredTransactions.slice(0, +limit);
+    }
+    if (offset) {
+      filteredTransactions = filteredTransactions.slice(+offset);
+    }
+    res.json(filteredTransactions);
+
+    //Handle errors
+    if (response.data.status === '0') {
+      console.log(response.data.status);
+      return { status: 404, message: response.data.result };
+    } else {
+      return { status: 200, message: response.data };
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+module.exports = router;
